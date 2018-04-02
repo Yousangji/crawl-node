6abeef7ddd227a86544002eb2f5be33b9bdcef2f
Sangji Yoo <sangji.yoo@kono.ai>
2018-04-02 17:58:02 +0900

    feat(teamInfoUtil.js) : added data schema (TeamInfo, TokenInfo, Whitepaper Address, Social Links of Team, TeamMemberSocialLinks)

--- teamInfoUtil.js
+++ teamInfoUtil.js
@@ -2,48 +2,79 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 const basicUrl = "https://icobench.com/icos";
 
-
-const getTeamInfo = (subUrl) => axios({
-    method: 'get',
-    host: 'icobench.com',
-    url: basicUrl.substr(0, basicUrl.lastIndexOf("/")) + subUrl
-})
-    .then((response) => {
-
-
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const teamInfo = [];
-
-
-        //get Name of The Team
-        const teamName = $('.name').find('h1').text();
+/**
+ *
+ * @param subUrl
+ * @returns {Promise<T>}
+ */
+const getTeamInfo = (subUrl) => {
+    return axios({
+        method: 'get',
+        host: 'icobench.com',
+        url: basicUrl.substr(0, basicUrl.lastIndexOf("/")) + subUrl
+    })
+        .then((response) => {
 
 
-        //get TeamMember & Advisor ETC..(Board, Expert)
-        const $rowsOfGroup = $('#team > .row');
-        $rowsOfGroup.each(function () {
-            const groupRole = $(this).prev('h3').text()!=""? $(this).prev('h3').text() : "TeamMember"
+            const html = response.data;
+            const $ = cheerio.load(html);
+
+
+
+            //get Info of The Team
+
+            //------------------get Names (Team name, Coin Name)
+            const teamName = $('.name > h1').text();
+
+            //-------------------get WhitePaper
+            const whitePaperAddr = $('.tabs').find("[href$='.pdf']").attr('href');
+
+            //-------------------get Team social link
+            const socialLinks = [];
+            const $teamSocialLink = $('.fixed_data > .socials > a');
+            $teamSocialLink.each(function (index){
+                const social = $(this).attr('class');
+                const link = $(this).attr('href');
+                socialLinks[index] = {social , link};
+            });
+
+
+            //-------------------get TeamMember & Advisor ETC..(Board, Expert)
+            const teamMembersInfo = [];
+            const $rowsOfGroup = $('#team > .row');
+            $rowsOfGroup.each(function () {
+                const groupRole = $(this).prev('h3').text() !== "" ? $(this).prev('h3').text() : "TeamMember";
                 const $groupMembers = $(this).children('div');
-                const groupMemberArr =[];
+                const groupMemberArr = [];
 
+                //get MetaData of Member
                 $groupMembers.each(function (index) {
                     const name = $(this).find('h3').text();
                     const linkedinAddr = $(this).find('.linkedin').attr('href');
                     const socialLink = $(this).find('.socials').children(":not(:contains('LinkedIn'))").attr('href');
                     const position = $(this).find('h4').text();
-                    groupMemberArr[index]={name, linkedinAddr, socialLink, position};
+                    groupMemberArr[index] = {name, linkedinAddr, socialLink, position};
                 });
 
-            teamInfo.push({[groupRole]:groupMemberArr});
+                teamMembersInfo.push({[groupRole]: groupMemberArr});
 
+            });
+
+            //------------------get Token Info
+            const tokenInfo = {};
+            const $tokenInfo = $('.box_left > .row');
+            $tokenInfo.each(function (){
+                const infoLabel = $(this).children('.label').text();
+                const infoValue = $(this).children('.value').text();
+                tokenInfo[infoLabel]=infoValue;
+            });
+
+            console.log(JSON.stringify({teamName, tokenInfo, whitePaperAddr, socialLinks, teamMembersInfo},null , 4));
+        })
+        .catch(function (error) {
+            console.log(error);
         });
-
-        console.log(JSON.stringify({teamName, teamInfo}));
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+};
 
 
 const getICOLinks = () => axios.get(basicUrl)
@@ -51,12 +82,12 @@ const getICOLinks = () => axios.get(basicUrl)
         if (response.status === 200) {
             const html = response.data;
             const $ = cheerio.load(html);
-            var links = [];
+            const links = [];
             ($('.name').each(function () {
                 links.push($(this).attr('href'));
             }));
 
-            for (var i = 0; i < links.length; i++) {
+            for (let i = 0; i < links.length; i++) {
                 getTeamInfo(links[i]);
             }
         }
@@ -64,5 +95,4 @@ const getICOLinks = () => axios.get(basicUrl)
 
 module.exports = {
     getICOLinks
-
 };
